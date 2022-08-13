600ca90193f16e5e5bd9904b95bc9f63a3bd56b0
ul1ra <benpetercunningham@gmail.com>
2022-08-13 19:05:09 +0200

    archieved showcase functionalitty for later

---
+++ src/archieve/Card-fucntionality.jsx
@@ -0,0 +1,112 @@
+import { Component } from "react";
+import { CardData } from "./CardInfo.jsx";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+import Swipe from "react-easy-swipe";
+
+
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSlide: 0,
+      paused: false,
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      if (this.state.paused === false) {
+        let newSlide =
+          this.state.currentSlide === CardData.length - 1
+            ? 0
+            : this.state.currentSlide + 1;
+        this.setState({ currentSlide: newSlide });
+      }
+    }, 3000);
+  }
+
+  nextSlide = () => {
+    let newSlide =
+      this.state.currentSlide === CardData.length - 1
+        ? 0
+        : this.state.currentSlide + 1;
+    this.setState({ currentSlide: newSlide });
+  };
+
+  prevSlide = () => {
+    let newSlide =
+      this.state.currentSlide === 0
+        ? CardData.length - 1
+        : this.state.currentSlide - 1;
+    this.setState({ currentSlide: newSlide });
+  };
+
+  setCurrentSlide = (index) => {
+    this.setState({ currentSlide: index });
+  };
+
+  render() {
+    return (
+      <div className="mt-8">
+        <div className="max-w-lg h-72 flex overflow-hidden relative">
+          <AiOutlineLeft
+            onClick={this.prevSlide}
+            className="absolute left-0 text-3xl inset-y-1/2 text-white cursor-pointer"
+          />
+
+          <Swipe onSwipeLeft={this.nextSlide} onSwipeRight={this.prevSlide}>
+            {CardData.map((slide, index) => {
+              return (
+                <img
+                  src={slide.image}
+                  alt="This is a carousel slide"
+                  key={index}
+                  className={
+                    index === this.state.currentSlide
+                      ? "block w-full h-auto object-cover"
+                      : "hidden"
+                  }
+                  onMouseEnter={() => {
+                    this.setState({ paused: true });
+                  }}
+                  onMouseLeave={() => {
+                    this.setState({ paused: false });
+                  }}
+                />
+              );
+            })}
+          </Swipe>
+
+          <div className="absolute w-full flex justify-center bottom-0">
+            {CardData.map((element, index) => {
+              return (
+                <div
+                  className={
+                    index === this.state.currentSlide
+                      ? "h-2 w-2 bg-blue-700 rounded-full mx-2 mb-2 cursor-pointer"
+                      : "h-2 w-2 bg-white rounded-full mx-2 mb-2 cursor-pointer"
+                  }
+                  key={index}
+                  onClick={() => {
+                    this.setCurrentSlide(index);
+                  }}
+                ></div>
+              );
+            })}
+          </div>
+
+          <AiOutlineRight
+            onClick={this.nextSlide}
+            className="absolute right-0 text-3xl inset-y-1/2 text-white cursor-pointer"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
+
+{/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="bg-primary scale-y-180">
+        <path fill="#FFFFFF" fill-opacity="1" d="M0,96L48,80C96,64,192,32,288,21.3C384,11,480,21,576,53.3C672,85,768,139,864,176C960,213,1056,235,1152,202.7C1248,171,1344,85,1392,42.7L1440,0L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+      </svg> */}
---
+++ src/archieve/CardInfo.jsx
@@ -0,0 +1,52 @@
+import Introfly from "../../../assets/projects/introfly.png";
+import Gradient from "../../../assets/projects/gradient.png";
+import Fusion from "../../../assets/projects/fusion.png";
+import AnimalFarm from "../../../assets/projects/animalFarm.png";
+import MemoryGame from "../../../assets/projects/memoryGame.png";
+
+export const CardData = [
+  {
+    title: "Introfly",
+    description: "An app that simplifies meetings by allowing users to create a short video using their webcam to introduce themselves and introduce the meeting using their Google account. You can create an event and invite others to view and create their own intro videos.",
+    techStack: "React, Redux, Python, Django, Docker, Nginx, Tailwind CSS, Gitlab, PostgreSQL, Gmail API, Google Calendar API, OAuth 2.0",
+    link: "https://gitlab.com/OndrejJanosik777/introfly",
+    image: { Introfly },
+  },
+  {
+    title: "Gradient",
+    description: "This project is a clone-like Instagram SPA where you can login and create an account, see your friends and posts that you create with your profile.",
+    techStack: "React, Redux, Python, Django, Docker, Nginx, Sass, Gitlab, PostgreSQL",
+    link: "https://github.com/Ul1ra/Gradient",
+    image: { Gradient },
+  },
+  {
+    title: "Fusion",
+    description: "This project represents a restaurant review portal for restauranters and individuals to rate create & review restaurants like Yelp",
+    techStack: "React, Redux, Python, Django, Docker, Nginx, Sass, Gitlab, PostgreSQL",
+    link: "https://github.com/Ul1ra/Fusion",
+    image: { Fusion },
+  },
+  {
+    title: "Animal Farm",
+    description: "A simple app using Express.js as an API & server to create random animals, which has a search field on the React to search through the data of what you typed in.",
+    techStack: "React, Express.js",
+    link: "https://github.com/Ul1ra/animal_farm",
+    image: { AnimalFarm },
+  },
+  {
+    title: "Memory Game",
+    description: "A quick example of a memory game where you need to match two tiles together",
+    techStack: "React, Firebase, PWA",
+    link: "https://github.com/Ul1ra/memorygame",
+    image: { MemoryGame },
+  },
+];
+
+export const ResumeData = [
+  {
+    title: "Resume",
+    description: "A portfolio to show myself in an artisitc way!",
+    techStack: "React, Tailwind",
+    link: "https://github.com/Ul1ra/resume",
+  },
+];
